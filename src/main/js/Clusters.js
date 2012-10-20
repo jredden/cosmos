4b95388dd7d479aa7383850b7aef15ee0347ffb3
jredden <jredden@jredden-laptop.(none)>
2012-10-19 21:34:19 -0700

    clusters WIP

---
+++ src/main/js/Clusters.js
@@ -0,0 +1,14 @@
+var clusterAttributes = (function(){
+	
+	// private
+	
+	var clusterColors = ["#DAA520","#BA55D3","#FF6347","#7FFF00","#70DBDB"];
+	
+	// public
+	
+	return {
+		drawOneCluster: function draw(jsGraphic, distance, scale, angle){
+			
+		}
+	};
+})();
